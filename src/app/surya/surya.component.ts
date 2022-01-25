ba18a8dcbe2f0163ed5c8fb0f7e23e7d35ee6f6c
Surya Venkatesan <suryaedu71@gmail.com>
2022-01-25 17:11:04 +0530

    Basica el spano

--- src/app/surya/surya.component.ts
+++ src/app/surya/surya.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./surya.component.css']
 })
 export class SuryaComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
-
+  // should not placed inside ngOnInit()!!
+  name = 'Surya';
+  age = 20;
+  cname = "red";
+  fweight = "italic";
+  bstyle = "10px solid green";
+  birthday = new Date(2002, 0, 26);
+  container_type = 'btn btn-primary'
+  cricketers = [{
+    name: 'Saurav Ganguly',
+    ODI: "21",
+    Test: "17"
+  }, {
+    name: 'Rohit sharma',
+    ODI: "21",
+    Test: "17"
+  },];
 }
