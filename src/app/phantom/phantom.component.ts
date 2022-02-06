fbe3bd83439f19d8b1e3ed99755d2359c8a36fd3
Surya Venkatesan <suryaedu71@gmail.com>
2022-02-06 19:07:06 +0530

    ADDED form validation

--- src/app/phantom/phantom.component.ts
+++ src/app/phantom/phantom.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
+import { FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-phantom',
   templateUrl: './phantom.component.html',
@@ -15,8 +17,15 @@ export class PhantomComponent implements OnInit {
   }
 
   submitFormToTSFile(theForm : any){
-
-
+    var genre = theForm.genre;
+    var language = theForm.language
+    var aval_langs = ['Tamil','English','Sindhi','Telugu']
+    console.log(genre);
+    if(aval_langs.includes(language)){
+      alert("Your language is available")
+    }else{
+      alert("The specified Language is unavailable. Please use subtitles.");
+    }
   }
 
 }
